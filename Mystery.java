01d2003abeeb9de3a5923c5c463279bbc2f570da
Gaurav Gupta <gaurav.gupta@mq.edu.au>
2021-08-17 16:32:44 +1000

    Update Mystery.java

--- Mystery.java
+++ Mystery.java
@@ -1,7 +1,5 @@
 public class Mystery {
-	public static void main(String[] args) {
-		double input = (int)(Math.random()*100)/10.0;
-		int output = (int)(input+0.5);
-		System.out.println(input+" -> "+output);
+	public static int highest(int a, int b, int c) {
+		return 0;
 	}
 }
